4becc09ad93b36f8cdc1eae9ca3c56702e63126b
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-02-21 16:52:48 -0500

    Finished Intro page for now

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import backgroundVideo from '../src/assets/home-background.mp4'
+import introImage from '../src/assets/Intro-image-1.jpg'
 
 import './HomePage.css';
 
@@ -12,7 +13,7 @@ class HomePage extends Component{
 
     //State initilization
     this.state = {
-
+        yPos: window.pageYOffset
     };
 
     //Binding functions
@@ -37,7 +38,22 @@ class HomePage extends Component{
               </div>
 
               <div id="Intro-content">
-
+                <p id="Intro-text">
+                  I am currently a second year undergraduate student at the University of Toronto
+                  studying to graduate with a specialist in computer science and a major in mathematics. 
+                  My goal is to become a full-stack developer.
+                  <br/><br/>
+                  I enjoy coding and do a lot of it on the side. I have working knowledge with 
+                  both front and back end technologies. On the front-end, I have worked with ReactJS,
+                  Java Script and a couple CSS frameworks like SASS. I am more experienced with
+                  the back-end of things, working with API creation libraries such as Flask and
+                  express.js, databasing with Firebase and MongoDB, and utilizing cloud platforms
+                  such as AWS, Google Cloud Services, etc.
+                  <br/><br/>
+                  I am looking for 2020 summer internships at the moment, if you're interested you
+                  can look at my <a href="https://github.com/EstaticShark">resume.</a>
+                </p>
+                <img id="Intro-picture" src={introImage} alt="Cannot be loaded"/>
               </div>
           </div>
     );
