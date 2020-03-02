5f4f2528e89166d7091104964cc10d3cd70c4899
Martin Chak <Martin.Chak@mail.utoronto.ca>
2020-03-01 22:47:26 -0500

    Updates

---
+++ precache-manifest.fb18133bb15eed089824c39b7c2847df.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e34811f063c649c6a84a52b0148f152",
+    "url": "/portfolio-website/index.html"
+  },
+  {
+    "revision": "e7abd4ffb680a9907043",
+    "url": "/portfolio-website/static/css/main.724bae69.chunk.css"
+  },
+  {
+    "revision": "5782631d42328094da62",
+    "url": "/portfolio-website/static/js/2.44320ccc.chunk.js"
+  },
+  {
+    "revision": "65c6a3bcd377c6e2495629791edc7694",
+    "url": "/portfolio-website/static/js/2.44320ccc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7abd4ffb680a9907043",
+    "url": "/portfolio-website/static/js/main.91414141.chunk.js"
+  },
+  {
+    "revision": "4dadb502b195c709763d",
+    "url": "/portfolio-website/static/js/runtime-main.b48eeadf.js"
+  },
+  {
+    "revision": "73690283439d613935f6c67409be76db",
+    "url": "/portfolio-website/static/media/Intro-image-1.73690283.jpg"
+  },
+  {
+    "revision": "510cdcbcee61ed77a5d63caca6484040",
+    "url": "/portfolio-website/static/media/Stem-notes-1.510cdcbc.PNG"
+  },
+  {
+    "revision": "3b41773c5baafca1d407179a74b46b86",
+    "url": "/portfolio-website/static/media/Stem-notes-2.3b41773c.PNG"
+  },
+  {
+    "revision": "2bcfbb1c5c8359c09b46f24b4f0fdd87",
+    "url": "/portfolio-website/static/media/Stem-notes-3.2bcfbb1c.png"
+  },
+  {
+    "revision": "fe039989804c513c76b69bb6fcd50f95",
+    "url": "/portfolio-website/static/media/home-background.fe039989.mp4"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-site/precache-manifest.d89ec92169f23053c959399cc9399a40.js"
+  "/portfolio-website/precache-manifest.fb18133bb15eed089824c39b7c2847df.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-site/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-website/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
